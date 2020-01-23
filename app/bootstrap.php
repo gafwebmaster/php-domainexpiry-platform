4ab65c226b3fb825d993f501176807b4caf1c0e4
Gabriel <gafwebmaster@users.noreply.github.com>
2020-01-24 01:40:24 +0200

    Update bootstrap.php

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,1 +1,18 @@
+<?php
 
+//Config
+require_once '../app/config/config.php';
+
+//Load helpers
+require_once 'helpers/url_helper.php';
+require_once 'helpers/session_helper.php';
+require_once 'helpers/ip_helper.php';
+require_once 'helpers/domainToId_helper.php';
+require_once 'helpers/calculateDays_helper.php';
+require_once 'helpers/passwordValidate_helper.php';
+require_once 'libraries/PHPMailer/Email.php';
+
+//Autoload core libraries
+spl_autoload_register(function($className) {
+    require_once 'libraries/' . $className . '.php';
+});
